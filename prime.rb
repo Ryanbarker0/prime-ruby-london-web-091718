5d68aa019796a015812602346e5b1c15b8d29014
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-10 15:48:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   if number < 2
     return false
   else
-    (2..number - 1).to_a.all do |check_prime|
+    (2..number - 1).to_a.all? do |check_prime|
       number % check_prime != 0
     end
   end
